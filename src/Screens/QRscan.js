2260ad316a45a220d99f9290f4ac934d5aeceaa1
watthell234 <gonzalezjulius.c@gmail.com>
2020-06-17 22:03:35 -0400

    try out getting id from QR codev2

--- src/Screens/QRscan.js
+++ src/Screens/QRscan.js
@@ -21,7 +21,7 @@ export default function App() {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data['carpool_id']} has been scanned!`);
+    alert(`Bar code with type ${type} and data ${data.carpool_id} has been scanned!`);
     // http.post('/slug', data['carpool_id']).then(() => this.props.navigation.navigate('Position'))
 
   };
