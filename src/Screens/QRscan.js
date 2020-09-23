08a49c81592fe2bfdf7d8d8ba5f56118a6d9ed7b
watthell234 <gonzalezjulius.c@gmail.com>
2020-09-23 02:01:30 -0400

    cleanup QRreader code

--- src/Screens/QRscan.js
+++ src/Screens/QRscan.js
@@ -78,70 +78,6 @@ class QRReader extends React.Component {
 
 }
 
-// function QRReaderScreen({navigation}) {
-//   const [hasPermission, setHasPermission] = useState(null);
-//   const [scanned, setScanned] = useState(false);
-//
-//   useEffect(() => {
-//     (async () => {
-//       const { status } = await BarCodeScanner.requestPermissionsAsync();
-//       setHasPermission(status === 'granted');
-//     })();
-//   }, []);
-//
-//   const handleBarCodeScanned = ({ type, data }) => {
-//     setScanned(true)
-//     data = JSON.parse(data)
-//     // alert(`Bar code with type ${type} and Slug id: ${data.slug_id} has been scanned!`);
-//     carQr = data.car_qr
-//     http.post('/checkin', {carQr})
-//     .then(() => navigation.navigate('Position'))
-//     .catch((err) => console.log(err))
-//
-//   };
-//
-//   if (hasPermission === null) {
-//     return <Text>Requesting for camera permission</Text>;
-//   }
-//   if (hasPermission === false) {
-//     return <Text>No access to camera</Text>;
-//   }
-//
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         flexDirection: 'column',
-//         justifyContent: 'flex-end',
-//       }}>
-//       <BarCodeScanner
-//         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-//         style={[StyleSheet.absoluteFillObject, styles.container]}>
-//         <Text style={styles.description}>HitchIn</Text>
-//         <Image
-//           style={styles.qr}
-//           source={require('./assets/qr-scanner.png')}
-//         />
-//       </BarCodeScanner>
-//
-//       {scanned && <Button title={'Tap to Scan Again'}
-//       onPress={() => setScanned(false)} />}
-//     </View>
-//   );
-// }
-//
-// function StartRide() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//     <TouchableOpacity
-//         style={styles.button}
-//         onPress={() => {}}>
-//         <Text style={{color: "#FFFFFF"}}>Start Trip</Text>
-//     </TouchableOpacity>
-//     </View>
-//   );
-// }
-
 class StartRide extends React.Component {
   render() {
     return (
@@ -157,37 +93,6 @@ class StartRide extends React.Component {
 }
 
 
-// function MyTabs() {
-//   return (
-//     <Tab.Navigator
-//       initialRouteName="QRReaderScreen"
-//       tabBarOptions={{
-//         activeTintColor: '#e91e63',
-//       }}
-//     >
-//       <Tab.Screen
-//         name="QRReaderScreen"
-//         component={QRReaderScreen}
-//         options={{
-//           tabBarLabel: 'Rider',
-//           // tabBarIcon: ({ color, size }) => (
-//           //   <MaterialCommunityIcons name="home" color={color} size={size} />
-//           // ),
-//         }}
-//       />
-//       <Tab.Screen
-//         name="StartRide"
-//         component={StartRide}
-//         options={{
-//           tabBarLabel: 'Driver',
-//           // tabBarIcon: ({ color, size }) => (
-//           //   <MaterialCommunityIcons name="bell" color={color} size={size} />
-//           // ),
-//         }}
-//       />
-//       </Tab.Navigator>
-//   );
-// }
 const Tab = createBottomTabNavigator();
 
 class MyTabs extends React.Component {
@@ -212,13 +117,6 @@ export default function App() {
 }
 
 
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <MyTabs />
-//     </NavigationContainer>
-//   );
-// }
 
 const styles = StyleSheet.create({
   container: {
