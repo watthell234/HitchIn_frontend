2ab0418cbd08fb62c05b9e1bca8093595e81f867
watthell234 <gonzalezjulius.c@gmail.com>
2021-01-16 00:18:20 -0500

    created 'protected' screens and check for tokens

--- App.js
+++ App.js
@@ -11,7 +11,8 @@ import Position from './src/Screens/Position';
 import PairingScreen from './src/Screens/Pairing';
 import QRReader from './src/Screens/QRscan';
 import CarForm from './src/Screens/CarForm';
-import EndTripScreen from './src/Screens/EndTrip'
+import EndTripScreen from './src/Screens/EndTrip';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -28,52 +29,58 @@ class QRTabs extends React.Component {
 }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userToken: null
+    }
+  }
+
+  async getToken(token) {
+    try {
+      let getToken = await AsyncStorage.getItem("authToken");
+      let userToken = JSON.parse(getToken);
+      console.log(userToken);
+      this.setState({userToken: userToken})
+    } catch (error) {
+      console.log("Something went wrong", error);
+    }
+  }
+
+  componentDidMount() {
+    this.getToken();
+  }
+
   render() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Init">
-          <Stack.Screen name="Init" component={InitScreen} options={{ title: 'Welcome' }} />
-          <Stack.Screen name="CarpoolRoute" component={CarpoolRoute} />
-          <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Sign Up' }} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="CreateProfile" component={CreateProfileScreen} />
-          <Stack.Screen name="Position" component={Position} />
-          <Stack.Screen name="Pairing" component={PairingScreen} />
-          <Stack.Screen name="CarForm" component={CarForm}options={{ title: 'Car' }}/>
-          <Stack.Screen name="QRTabs" component={QRTabs} />
-          <Stack.Screen name="EndTrip" component={EndTripScreen} />
+        <Stack.Navigator>
+        { this.state.userToken == null ? (
+          <>
+            <Stack.Screen name="Init" component={InitScreen} options={{ title: 'Welcome' }} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Sign Up' }} />
+            <Stack.Screen name="CarForm" component={CarForm} options={{ title: 'Car' }}/>
+            <Stack.Screen name="QRTabs" component={QRTabs} />
+            <Stack.Screen name="Pairing" component={PairingScreen} />
+            <Stack.Screen name="Position" component={Position} />
+            <Stack.Screen name="EndTrip" component={EndTripScreen} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="QRTabs" component={QRTabs} />
+            <Stack.Screen name="Pairing" component={PairingScreen} />
+            <Stack.Screen name="Position" component={Position} />
+            <Stack.Screen name="EndTrip" component={EndTripScreen} />
+          </>
+        ) }
         </Stack.Navigator>
       </NavigationContainer>
     );
   }
 }
 
-// async getToken(token) {
-//   try {
-//     let getToken = await AsyncStorage.getItem("authToken");
-//     let token = JSON.parse(getToken);
-//     console.log(token);
-//     this.setState({token: token})
-//   } catch (error) {
-//     console.log("Something went wrong", error);
-//   }
-// }
 
-// state.userToken == null ? (
-//   <>
-//     <Stack.Screen name="Init" component={InitScreen} options={{ title: 'Welcome' }} />
-//     <Stack.Screen name="Login" component={LoginScreen} />
-//     <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Sign Up' }} />
-//   </>
-// ) : (
-//   <>
-//     <Stack.Screen name="CarForm" component={CarForm}options={{ title: 'Car' }}/>
-//     <Stack.Screen name="QRTabs" component={QRTabs} />
-//     <Stack.Screen name="Pairing" component={PairingScreen} />
-//     <Stack.Screen name="Position" component={Position} />
-//     <Stack.Screen name="EndTrip" component={EndTripScreen} />
-//   </>
-// )
 
 
 export default App;
