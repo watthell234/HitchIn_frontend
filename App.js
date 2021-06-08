abb61c54be9949a96bc77683a71ea0fa31add6d8
Joseph Lee <leethfc11@gmail.com>
2021-06-08 15:32:09 -0400

    Had to re-do App.js for switching between screens properly.

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Image, ImageBackground, Keyboard, TouchableOpacity} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { ActivityIndicator, StatusBar, StyleSheet, Text, View, Button, TextInput, Image, ImageBackground, Keyboard, TouchableOpacity} from 'react-native';
+import { createAppContainer, NavigationContainer, createBottomTabNavigator, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 // import io from 'socket.io-client';
 import InitScreen from './src/Screens/InitPage';
 import SignUpScreen from './src/Screens/Signup';
@@ -15,72 +15,63 @@ import LoginScreen from './src/Screens/LoginPage';
 import Position from './src/Screens/Position';
 import PairingScreen from './src/Screens/Pairing';
 import QRReader from './src/Screens/QRscan';
-import CarForm from './src/Screens/CarForm';
+import CarFormScreen from './src/Screens/CarForm';
 import EndTripScreen from './src/Screens/EndTrip';
 import CarInfoScreen from './src/Screens/CarInfo';
 
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
+// const Tab = createBottomTabNavigator();
+// const Stack = createStackNavigator();
 
-function Root() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Init" component={InitScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="CreateProfile" component={CreateProfileScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-    </Stack.Navigator>
-  );
-}
+const RootStack = createStackNavigator({
+  Init: InitScreen,
+  Login: LoginScreen,
+  CreateProfile: CreateProfileScreen,
+  SignUp: SignUpScreen
+});
 
-function LoggedIn() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="CarpoolRoute" component={CarpoolRouteScreen} />
-      <Stack.Screen name="RideOrDrive" component={RideOrDriveScreen} />
-      <Stack.Screen name="CarInfo" component={CarInfoScreen} />
-      <Stack.Screen name="CarForm" component={CarForm} />
-      <Stack.Screen name="Pairing" component={PairingScreen} />
-      <Stack.Screen name="Position" component={Position} />
-      <Stack.Screen name="EndTrip" component={EndTripScreen} />
-    </Stack.Navigator>
-  );
-}
+const LoggedInStack = createStackNavigator({
+  CarpoolRoute: CarpoolRouteScreen,
+  RideOrDrive: RideOrDriveScreen,
+  CarInfo: CarInfoScreen,
+  CarForm: CarFormScreen,
+  Pairing: PairingScreen,
+  Position: Position,
+  EndTrip: EndTripScreen
+});
 
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userToken: null
-    }
-  }
-
-  async getToken(token) {
-    try {
-      let token = await AsyncStorage.getItem("authToken");
-      let userToken = JSON.parse(token);
-      console.log(userToken);
-      this.setState({userToken: userToken})
-    } catch (error) {
-      console.log("Something went wrong", error);
-    }
-  }
-
+class AuthLoadingScreen extends React.Component {
   componentDidMount() {
+    this._bootstrapAsync();
+  }
 
-    this.getToken();
+  async _bootstrapAsync() {
+    try {
+      let userToken = await AsyncStorage.getItem("authToken");
+      this.props.navigation.navigate(userToken ? 'LoggedIn' : 'Root')
+    } catch (error) {
+      console.log("Could not retrieve token", error);
+    }
   }
 
   render() {
-    const Drawer = createDrawerNavigator();
     return (
-      <NavigationContainer>
-        <Drawer.Navigator>
-          <Drawer.Screen name="Root" component={Root} />
-          <Drawer.Screen name="LoggedIn" component={LoggedIn} />
-        </Drawer.Navigator>
-      </NavigationContainer>
-    );
+      <View>
+        <ActivityIndicator />
+        <StatusBar barStyle='default' />
+      </View>
+    )
   }
 }
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      AuthLoading: AuthLoadingScreen,
+      Root: RootStack,
+      LoggedIn: LoggedInStack,
+    },
+    {
+      initialRouteName: 'AuthLoading',
+    }
+  )
+);
