91caa9f44e93298d3917640dc2282c6b2c6f156b
Julius Gonzalez <watthell234@users.noreply.github.com>
2021-07-27 18:53:54 -0400

    change debug to false because sentry docs said to in prod

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ import EndTripScreen from './src/Screens/EndTrip';
 Sentry.init({
   dsn: 'https://ec5caaabba9b489686c9f1768c117b62@o931327.ingest.sentry.io/5880255',
   enableInExpoDevelopment: true,
-  debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+  debug: false, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
 });
 
 Sentry.Native.nativeCrash();
